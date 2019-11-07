90c6a19494a474b1dc96ab4bf376060a5c563e84
Adrian Castello <adcastel@upv.es>
2019-11-07 17:02:37 +0100

    blis calls added in general scenario

--- malleable_dnn.c
+++ malleable_dnn.c
@@ -76,6 +76,7 @@ int count_layers(FILE *fp){
 
 // Esta funcion es para hacer profile, para la version final se llamara
 //a my_gemm()
+
 void test_gemm(dim_t m, dim_t n, dim_t k, int max_threads, rntm_t * rnmt ){
 
     printf("GEMM %dx%dx%d\n", m, n, k);    
@@ -139,20 +140,127 @@ void test_gemm(dim_t m, dim_t n, dim_t k, int max_threads, rntm_t * rnmt ){
 
 }
 
-void my_gemm(){
-    
-                                
-
-}
-
-/* helper functions */
-int problem_size(int elements, int nprocs, int rank);
 
 /* Computation functions */
-void FC_gemm_fp(int m, int n, int k, float * A, int lda,
-        float * B, int ldb, float * C, int ldc, int threads, int max_threads, rntm_t * rntm);
-void CONV_fp(int l, int K, int B, int H, int W, int KH, int KW, int C,
-        float * I, float * IP, float * O, float * F, double * time, int threads, int max_threads, rntm_t * rntm);
+#ifdef TESTGEMM
+void FC_gemm_fp(int m, int n, int k, float * A, int lda, float * B, int ldb, float * C, int ldc, int threads, int max_threads, rntm_t * rntm) {
+#ifndef NOGEMM  
+    test_gemm(m,n,k,max_threads,rntm);
+#endif
+}
+#else
+void FC_gemm_fp(obj_t * a, obj_t *b, obj_t *c, obj_t * alpha, obj_t * beta ,rntm_t * rntm){
+    bli_gemm_ex(alpha, a, b, beta, c, NULL, rnmt);
+}
+#endif
+
+
+void CONV_fp(int l, int K, int B, int H, int W, int KH, int KW, int C, float * I, 
+        float * IP, obj_t *a, obj_t * F, obj_t * O, obj_t * alpha, obj_t * beta, 
+        double * time, int threads, int max_threads, rntm_t * rntm) {
+
+    // B batch size
+    // Input image of size H x W, with C channels
+    // K Kernels of size KH x KW each, with C channels
+    /*
+    float I[C][B][H][W];          // Input: C x (B H W)
+    float IP[C][KW][KH][B][H][W]; // Patched input: (C K_H K_W) x (B H W)
+    float O[K][B][H][W];          // Output: K x (B H W)
+    float F[K][C][KH][KW];        // Filter: K x (C K_H K_W)
+     */
+
+    int b, h, w, kh, kw, c;
+#ifdef TIMER
+    *time = omp_get_wtime();
+#endif
+    // Im2col: I -> IP
+
+    int kk1 = KH * KW * B * H*W;
+    int  kk2 = KW * B * H*W;
+    int  kk3 = B * H*W;
+    int kk4 = H*W;
+    int kk5 = B * (H + KH)*(W + KW);
+    int kk6 = (H + KH)*(W + KW);
+    int kk7 = (W + KW);
+    int jk1, ik1, ik2, jk2, jk3, jk4, ik3, ik4, ik5;
+#ifndef NOIM2COL
+
+#pragma omp parallel for private(b,h,w,kh,kw,ik1,ik2,ik3,ik4,ik5,jk1,jk2,jk3,jk4) num_threads(threads)
+    for (c = 0; c < C; c++) {
+        ik1 = c*kk1;
+        jk1 = c*kk5;
+        for (b = 0; b < B; b++) {
+            ik2 = ik1 + b*kk4;
+            jk2 = jk1 + b*kk6;
+            for (kh = 0; kh < KH; kh++) {
+                ik3 = ik2 + kh*kk2;
+                jk3 = jk2 + (h + kh) * kk7;
+                for (kw = 0; kw < KW; kw++) {
+                    ik4 = ik3 + kw * kk3;
+                    jk4 = jk3 + kw;
+                    for (h = 0; h < H; h++) {
+                        ik5 = ik4 + h*W;
+                        for (w = 0; w < W; w++){
+                           IP[ ik5 + w ] = I[ /*jk4 +*/ w];
+			}
+                    }
+                }
+            }
+        }
+    }
+
+#endif
+
+#ifdef TIMER
+    *time = omp_get_wtime() - *time;
+#endif
+    // Gemm
+
+    int m = K;
+    int n = B * H*W;
+    int k = C * KH*KW;
+    int lda = m;
+    int ldb = k;
+    int ldc = m;
+
+#ifndef NOGEMM  
+    /*cblas_sgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
+            m, n, k, 1,
+            F, lda, IP, ldb, 0, O, ldc);*/
+    //test_gemm(m,n,k,max_threads,rntm);
+    bli_gemm_ex(alpha, a, F, beta, O, NULL, rnmt);
+#endif
+//printf("despues de gemm\n");
+
+    //printf("FP CONV GEMM m(%d) : n(%d)=b(%d).h(%d).w(%d) : k(%d)=c(%d).kh(%d).kw(%d)\n", m, n, B, H, W, k, C, KH, KW);
+
+    /*free(miI);
+    free(miIP);
+    free(miO);
+    free(miF);*/
+    /*GEMM -> O = F * IP
+
+            No transpose, No transpose
+
+            alpha = 1.0
+
+            beta  = 0.0
+            m = K, n = (B H W), k = (C K_H K_W) 
+     */
+}
+
+
+int problem_size(int elements, int nprocs, int rank) {
+    int part = elements / nprocs;
+    int rest = elements % nprocs;
+    if (rest > 0) {
+        if (rank < rest) {
+            part++;
+        }
+    }
+    return part;
+}
+
 
 /* Communication functions */
 
@@ -163,7 +271,7 @@ int main(int argc, char * argv []) {
     double time = 0.0; 
     
     if (argc < 4 ){
-      perror("Usage: ./dnn model.csv steps teams [num_threads] {batch_size]\n");
+      perror("Usage: ./dnn model.csv steps teams [num_threads] [batch_size]\n");
       exit(-1);
     }
 
@@ -184,7 +292,6 @@ int main(int argc, char * argv []) {
     printf("Model: %s\n", argv[1]);
 #endif
     fp_model= fopen(argv[1], "r");
-    //printf("layers: %d\n",count_layers(fp_model));
     int NUM_LAYERS = count_layers(fp_model)-1; //we discard the info line
     
     fclose(fp_model);
@@ -200,13 +307,13 @@ int main(int argc, char * argv []) {
      int * hstrides = malloc(sizeof(int)*NUM_LAYERS);
      int * procs = malloc(sizeof(int)*NUM_LAYERS);
     char line[MAX_LEN]; 
-    i = 0;
     int minsfc = 512;
     int minsconv = 8;
-    //fclose(fp_model);
-    
+    int number_fc = 0;
+    int number_conv = 0;
     fp_model= fopen(argv[1], "r");
     fgets(line, MAX_LEN, fp_model);
+    i = 0;
     while(fgets(line, MAX_LEN, fp_model)){
       char* tmp = strdup(line);
       const char* typel = getfield(tmp, 2); 
@@ -224,10 +331,12 @@ int main(int argc, char * argv []) {
     }
     else if ( !strcmp(typel, "fc") ){ 
     	type[i] = FC;    min_size[i]= minsfc;   nkernels[i]= 0; 
-	    channels[i]= 1;  kwidth[i]= 0;   kheight[i]= 0;   image_size[i] = 0; 
+	    channels[i]= 1;  kwidth[i]= 0;   kheight[i]= 0;   image_size[i] = 0;
+            number_fc++;
 	  }
     else if ( !strcmp(typel, "conv") ){ 
     	type[i] = CONV;  min_size[i]= minsconv; nkernels[i]= channels[i];
+        number_conv++;
     } 
     else if ( !strcmp(typel, "apool") ){ 
     	type[i] = APOOL; min_size[i]= minsconv; nkernels[i]= 0; 
@@ -317,19 +426,7 @@ int main(int argc, char * argv []) {
 #ifdef PROGRESS
     printf("mi = %lu | mip = %lu | mo = %lu | mf = %lu\n", max_i, max_ip, max_o, max_f);
 #endif
-    /*float * conv_i = malloc(max_i * sizeof (float));
-    float * conv_ip = malloc(max_ip * sizeof (float));
-    float * conv_o = malloc(max_o * sizeof (float));
-    float * conv_f = malloc(max_f * sizeof (float));*/ 
-    /* The maximum size of the matrix model fits with the one of the matrix */
-    size_t model_size = (max_size_fc > max_size_conv) ? max_size_fc : max_size_conv; //This is the maximum size of the model layers
-    //float * model = malloc(model_size * sizeof (float));
-    /* The data size is equal to the neurons in the first layer per the batch size */
-    size_t data_size = nneurons[0] * BATCH_SIZE;
-    //float * data = malloc(data_size * sizeof (float));
-
-
-
+    
 
     /* Warm-up zone */
     int omp_warm;
@@ -349,15 +446,57 @@ int main(int argc, char * argv []) {
 	{
     //	printf("Thread %d con team de %d threads reservando memoria...\n",omp_get_thread_num(), threads);
 	
+        obj_t * a = malloc(sizeof(obj_t) * NUM_LAYERS);    
+        obj_t * b = malloc(sizeof(obj_t) * NUM_LAYERS);    
+        obj_t * c = malloc(sizeof(obj_t) * NUM_LAYERS);
+        
+        obj_t alpha, beta;
+        num_t dt_a, dt_b, dt_c, dt_alpha, dt_beta;
+    //dim_t m, n, k;
+        dt_a = BLIS_DOUBLE;    
+        dt_b = BLIS_DOUBLE;    
+        dt_c = BLIS_DOUBLE;    
+        dt_alpha = BLIS_DOUBLE;
+        dt_beta = BLIS_DOUBLE;
+        bli_obj_create( dt_alpha, 1, 1, 0, 0, &alpha ); 
+        bli_obj_create( dt_beta,  1, 1, 0, 0, &beta );  
+        
+        int o;
+        for(o = 0; o < NUM_LAYERS; o++){
+            dim_t m, n, k;
+            if(type[l] == FC){
+                m = nneurons[l]; //nneurons[l]/procs[l];//antes /size
+                n = BATCH_SIZE;
+                k = nneurons[l - 1];
+                
+                             //We need to reshape if the previous one was CONV            
+            }
+            else{
+                if(type[l] == CONV){
+                    m = nkernels[l];
+                    n = BATCH_SIZE * image_size[l - 1]*image_size[l - 1];
+                    k = channels[l - 1] * kheight[l] * kwidth[l];
+                    
+                    
+                }
+            }
+                    bli_obj_create( dt_a, m, k, 0, 0, &a[o] );         
+                    bli_obj_create( dt_b, k, n, 0, 0, &b[o] );         
+                    bli_obj_create( dt_c, m, n, 0, 0, &c[o] );         
+                                                
+                    bli_randm( &a[o] );                                
+                    bli_randm( &b[o] );                                
+                    bli_randm( &c[o] );          
+                                                
+        }                                        
+        bli_setsc(  (0.9/1.0), 0.2, &alpha );           
+        bli_setsc(  (1.0/1.0), 0.0, &beta );            
+
+        
+        
     	float * conv_i = malloc(max_i * sizeof (float));
     	float * conv_ip = malloc(max_ip * sizeof (float));
-    	float * conv_o = malloc(max_o * sizeof (float));
-    	float * conv_f = malloc(max_f * sizeof (float)); 
-        
-    	float * matrix_A = malloc(max_size_fc * sizeof ( float));
-    	float * matrix_B = malloc(max_size_fc * sizeof ( float));
-    	float * matrix_C = malloc(max_size_fc * sizeof ( float));
-	
+    	
 	#pragma omp for private(l)
         for (s = 0; s < nsteps; s++) {
 #ifdef PROGRESS
@@ -378,16 +517,23 @@ int main(int argc, char * argv []) {
                 		printf("FC \n");
 #endif
                 		printf("FC \n");
+#ifdef TESTGEMMS
+
                             int m = nneurons[l]; //nneurons[l]/procs[l];//antes /size
                             int n = BATCH_SIZE;
                             int k = nneurons[l - 1]; //We need to reshape if the previous one was CONV
                             int lda = m;
                             int ldb = k;
                             int ldc = m;
+#endif
 #ifdef TIMER
                             fp_comp_timer[s][l] = omp_get_wtime();
 #endif
+#ifdef TESTGEMMS
                             FC_gemm_fp(m, n, k, matrix_A, lda, matrix_B, ldb, matrix_C, ldc, threads, max_threads, &rntm);
+#else
+                            FC_gemm_fp(&a[l],b[l],c[l], alpha, beta, &rntm);
+#endif
 #ifdef TIMER
                             fp_comp_timer[s][l] = omp_get_wtime() - fp_comp_timer[s][l];
                             m = nneurons[l];
@@ -405,7 +551,7 @@ int main(int argc, char * argv []) {
                                 int b = BATCH_SIZE;
                                 int h = image_size[l - 1];
                                 int w = image_size[l - 1];
-                                int c = channels[l - 1];
+                                int ch = channels[l - 1];
                                 int kh = kheight[l];
                                 int kw = kwidth[l];
 #ifdef TIMER
@@ -416,8 +562,10 @@ int main(int argc, char * argv []) {
 				size_t aux = 1;
 				printf("sizes mi = %lu | mip = %lu | mo = %lu | mf = %lu\n", c*b*h*w*aux, c*kw*kh*b*h*w*aux, num_kernels*b*h*w*aux, num_kernels*c*kw*kh*aux);
 #endif
-                                CONV_fp(l, num_kernels, b, h, w, kh, kw, c,
-                                    conv_i, conv_ip, conv_o, conv_f, &fp_im2col_timer[s][l],threads,max_threads, &rntm);
+                                CONV_fp(l, num_kernels, b, h, w, kh, kw, ch,
+                                    conv_i, conv_ip, &a[o], &b[o], &c[o], 
+                                        &alpha, &beta, &fp_im2col_timer[s][l],
+                                        threads,max_threads, &rntm);
 
 #ifdef TIMER
                                 fp_comp_timer[s][l] = omp_get_wtime() - fp_comp_timer[s][l];
@@ -493,148 +641,8 @@ int main(int argc, char * argv []) {
 
     printf("Total %d steps, batches %d, teams %d => time %f (s)\n", nsteps, BATCH_SIZE, teams, time);
 
-    /*free(matrix_A);
-    free(matrix_B);
-    free(matrix_C);*/
-    //   free(model);
-    //  free(data);
     return 0;
 }
 
-void FC_gemm_fp(int m, int n, int k, float * A, int lda, float * B, int ldb, float * C, int ldc, int threads, int max_threads, rntm_t * rntm) {
-    //mkl_domain_set_num_threads(threads, MKL_DOMAIN_BLAS);
-//	printf("FC con %d threads\n",threads);
-    //omp_set_num_threads(threads);
-  /*  #pragma omp parallel
-{
-	printf("Thread %d/%d En gemm \n",omp_get_thread_num(),omp_get_num_threads());
-}*/
-  //  printf("%d,%d,%d\n",m,n,k);
-#ifndef NOGEMM  
-  /*cblas_sgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
-          m, n, k, 1,
-           A, lda, B, ldb, 0, C, ldc);*/
-    test_gemm(m,n,k,max_threads,rntm);
-#endif
-    //printf("FP  FC  GEMM m(%d) : n(%d) : k(%d)\n", m, n, k);
-}
 
 
-void CONV_fp(int l, int K, int B, int H, int W, int KH, int KW, int C, float * I, float * IP, float * O, float * F, double * time, int threads, int max_threads, rntm_t * rntm) {
-
-    // B batch size
-    // Input image of size H x W, with C channels
-    // K Kernels of size KH x KW each, with C channels
-    /*
-    float I[C][B][H][W];          // Input: C x (B H W)
-    float IP[C][KW][KH][B][H][W]; // Patched input: (C K_H K_W) x (B H W)
-    float O[K][B][H][W];          // Output: K x (B H W)
-    float F[K][C][KH][KW];        // Filter: K x (C K_H K_W)
-     */
-
-//	printf("Conv con %d threads\n",threads);
-    int b, h, w, kh, kw, c;
-#ifdef TIMER
-    *time = omp_get_wtime();
-#endif
-    // Im2col: I -> IP
-
-    int kk1 = KH * KW * B * H*W;
-    int  kk2 = KW * B * H*W;
-    int  kk3 = B * H*W;
-    int kk4 = H*W;
-    int kk5 = B * (H + KH)*(W + KW);
-    int kk6 = (H + KH)*(W + KW);
-    int kk7 = (W + KW);
-    int jk1, ik1, ik2, jk2, jk3, jk4, ik3, ik4, ik5;
-//printf("Antes de im2col\n");
-#ifndef NOIM2COL
-
-#pragma omp parallel for private(b,h,w,kh,kw,ik1,ik2,ik3,ik4,ik5,jk1,jk2,jk3,jk4) num_threads(threads)
-    for (c = 0; c < C; c++) {
-//	printf("Thread %d/%d En im2Col it %d\n",omp_get_thread_num(),threads,c);
-        ik1 = c*kk1;
-        jk1 = c*kk5;
-        for (b = 0; b < B; b++) {
-            ik2 = ik1 + b*kk4;
-            jk2 = jk1 + b*kk6;
-            for (kh = 0; kh < KH; kh++) {
-                ik3 = ik2 + kh*kk2;
-                jk3 = jk2 + (h + kh) * kk7;
-                for (kw = 0; kw < KW; kw++) {
-                    ik4 = ik3 + kw * kk3;
-                    jk4 = jk3 + kw;
-                    for (h = 0; h < H; h++) {
-                        ik5 = ik4 + h*W;
-                        for (w = 0; w < W; w++){
-                           IP[ ik5 + w ] = I[ /*jk4 +*/ w];
-			}
-                    }
-                }
-            }
-        }
-    }
-
-#endif
-
-#ifdef TIMER
-    *time = omp_get_wtime() - *time;
-    /* char processor_name[MPI_MAX_PROCESSOR_NAME];
-       int name_len;
-       MPI_Get_processor_name(processor_name, &name_len);
-     double a = *time;    
-     printf("%s Layer %d FP space=%f time=%f  GB/s = %f\n",processor_name,l,C*B*H*W*KH*KW*2.0,a,(C*B*H*W*KH*KW*2.0*4.0/1000000000.0)/a);
-     */
-#endif
-    // Gemm
-
-    int m = K;
-    int n = B * H*W;
-    int k = C * KH*KW;
-    int lda = m;
-    int ldb = k;
-    int ldc = m;
-   // printf("%d,%d,%d\n",m,n,k);
-
-    //mkl_domain_set_num_threads(threads, MKL_DOMAIN_BLAS);
-    //omp_set_num_threads(threads);
-/*    #pragma omp parallel
-{
-	printf("Thread %d/%d En gemm de im2col \n",omp_get_thread_num(),omp_get_num_threads());
-}*/
-#ifndef NOGEMM  
-    /*cblas_sgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
-            m, n, k, 1,
-            F, lda, IP, ldb, 0, O, ldc);*/
-    test_gemm(m,n,k,max_threads,rntm);
-#endif
-//printf("despues de gemm\n");
-
-    //printf("FP CONV GEMM m(%d) : n(%d)=b(%d).h(%d).w(%d) : k(%d)=c(%d).kh(%d).kw(%d)\n", m, n, B, H, W, k, C, KH, KW);
-
-    /*free(miI);
-    free(miIP);
-    free(miO);
-    free(miF);*/
-    /*GEMM -> O = F * IP
-
-            No transpose, No transpose
-
-            alpha = 1.0
-
-            beta  = 0.0
-            m = K, n = (B H W), k = (C K_H K_W) 
-     */
-}
-
-
-int problem_size(int elements, int nprocs, int rank) {
-    int part = elements / nprocs;
-    int rest = elements % nprocs;
-    if (rest > 0) {
-        if (rank < rest) {
-            part++;
-        }
-    }
-    return part;
-}
